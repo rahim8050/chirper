98622ac75aafed2ffeca358f42fff21ee44ce697
rahim8050 <rahimranxx8050@gmail.com>
2024-12-12 16:14:39 +0000

    update on blades

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Chirp;
 use Illuminate\Http\Request;
 
+use Illuminate\Http\Response; 
+use Illuminate\View\View;
+
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): view
     {
         //
+        return view('chirps.index');
     }
 
     /**
